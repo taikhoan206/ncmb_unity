7dad754a8efe2d46cb720e01f725f323c1267deb
taikhoan206 <30455579+taikhoan206@users.noreply.github.com>
2017-11-15 11:51:29 +0700

    Add retry for build script

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -2,11 +2,24 @@
 project_path=$(pwd)/ncmb_unity
 # Log file path
 log_file=$(pwd)/TravisScripts/unity_build.log
-
+# Unity command path
+unity_command=/Applications/Unity/Unity.app/Contents/MacOS/Unity
+# Error code
 error_code=0
+build_error=1
+test_error=1
+# Config for retry 
+max_retry=1
+# Build and Test count
+build_count=0
+test_count=0
 
+
+# UNITY BUILD WITH RETRY 
+while [[ $build_count -lt $((max_retry+1)) && $build_error != 0 ]]
+do
 echo "* Building project for Mac OS."
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+$unity_command \
   -batchmode \
   -nographics \
   -silent-crashes \
@@ -14,22 +27,51 @@ echo "* Building project for Mac OS."
   -projectPath "$project_path" \
   -quit
 if [ $? = 0 ] ; then
-  echo "* Building Mac OS completed successfully."
-  error_code=0
+  echo "* Building Mac OS completed successfully" 
+  build_error=0
 else
-  echo "* Building Mac OS failed. Exited with $?."
-  error_code=1
+  echo "* Building Mac OS failed with $?"
+  build_error=1
 fi
+
+if [ $build_count -lt $((max_retry+1)) ]; then
+  build_count=$((build_count+1))
+fi
+
+done
 echo "* Unity build log"
 cat $log_file 
 
-test_result_file=$(pwd)/TravisScripts/test_runner_result.xml
+if [ $build_error != 0 ]; then
+  echo "x Building Mac OS completed failed. Retry: $((build_count-1))"
+  echo "x Please fix MacOS Building before running Test Runner"
+  exit $build_error
+fi
+
+# TEST RUNNER WITH RETRY 
+test_result_file=/Users/VITALIFY/ncmb_unity/TravisScripts/test_runner_result.xml
+while [[ $test_count -lt $((max_retry+1)) && $test_error != 0 ]]
+do
 echo "* Execute Test Runner"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+$unity_command \
 -runTests \
 -projectPath "$project_path" \
 -testResults "$test_result_file" \
 -testPlatform editmode
+
+failed=$(echo 'cat //test-run/@failed' | xmllint --shell $test_result_file | awk -F\" 'NR % 2 == 0 { print $2 }')
+
+if [[ -z $failed && $failed -gt 0 ]];
+then
+  test_error=2
+else 
+  test_error=0
+fi
+
+if [ $test_count -lt $((max_retry+1)) ]; then
+  test_count=$((test_count+1))
+fi
+done
 
 echo '* Test Runner result'
 cat $test_result_file 
@@ -38,26 +80,18 @@ total=$(echo 'cat //test-run/@total' | xmllint --shell $test_result_file | awk -
 passed=$(echo 'cat //test-run/@passed' | xmllint --shell $test_result_file | awk -F\" 'NR % 2 == 0 { print $2 }')
 failed=$(echo 'cat //test-run/@failed' | xmllint --shell $test_result_file | awk -F\" 'NR % 2 == 0 { print $2 }')
 
-if [ $failed -gt 0 ] 
-then
-  error_code=$((error_code+2))
+if [[ -z $total ]]; then
+  test_error=3
 fi
 
 echo "______________________________________________________________________"
-case "$error_code" in
-0)  echo "o Building Mac OS completed successfully."
-    echo "o Test Runner completed successfully [ Total:$total  Passed:$passed Failed:$failed ]" 
+echo "o Building Mac OS completed successfully. Retry: $((build_count-1))"
+case "$test_error" in
+0)  echo "o Test Runner completed successfully [ Total:$total  Passed:$passed Failed:$failed ]. Retry: $((test_count-1))"
     ;;
-1)  echo "x Building Mac OS completed failed."
-    echo "o Test Runner completed successfully [ Total:$total  Passed:$passed Failed:$failed ]"
+2)  echo "x Test Runner completed failed [ Total:$total  Passed:$passed Failed:$failed ]. Retry: $((test_count-1))"
     ;;
-2)  echo "o Building Mac OS completed successfully."
-    echo "x Test Runner completed failed [ Total:$total  Passed:$passed Failed:$failed ]"
-    ;;
-3)  echo "x Building Mac OS completed failed."
-    echo "x Test Runner completed failed [ Total:$total  Passed:$passed Failed:$failed ]"
+3)  echo "x Test Runner completed failed. Can not read xml result file"
     ;;
 esac
-exit $error_code
-
-
+exit $test_error
